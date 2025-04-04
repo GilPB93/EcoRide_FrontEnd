f2917c4ac6c23452d136d0a6e7aef5939417471f
GilPB93 <g.p.braga@hotmail.com>
2025-04-04 15:00:43 +0200

    Test Hello World on home page

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,8 +2,11 @@ import Route from "./Route.js";
 
 //Define here the routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
-    
+    new Route("/", "Accueil", "/pages/home.html"),
+
+
+];
+
 
 //Website's name : Route.title - websitename
 export const websiteName = "EcoRide";
