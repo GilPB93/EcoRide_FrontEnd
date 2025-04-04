9cf75424ea16f12fa8f626571a43f7324aa8bf22
GilPB93 <g.p.braga@hotmail.com>
2025-04-04 14:15:18 +0200

    Setup routing system & 404.html page

---
+++ Router/allRoutes.js
@@ -0,0 +1,8 @@
+import Route from "./Route.js";
+
+//Define here the routes
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html"),];
+
+//Website's name : Route.title - websitename
+export const websiteName = "EcoRide";
